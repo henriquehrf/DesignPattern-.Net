769a8a852ac56393c3b3ae35934e41b4fd79cb05
Henrique R. Firmino <henrique_rfirmino@hotmail.com>
2020-03-14 18:46:18 -0400

    Commit Inicial. Definição da estrutura inicial do projeto.

---
+++ DesignPattern-.Net/Foo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_.Net
+{
+	class Foo
+	{
+	}
+}
---
+++ FacadeTest/FooTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacadeTest
+{
+	[TestClass]
+	public class FooTest
+	{
+		[TestMethod]
+		public void FooTest1()
+		{
+		}
+	}
+}
